2898264b73e06a371e17433699bfa8a9bb70f212
Andrew Yang <andrew.henry.yang@gmail.com>
2022-11-02 20:45:56 -0700

    computer defense offense added

--- lesson_5/OoTTT.rb
+++ lesson_5/OoTTT.rb
@@ -26,18 +26,23 @@ class Board
     !!winning_marker # relies on truthiness of objects vs nil
   end
 
-  def count_human_marker(squares)
-    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
-  end
+  # def count_human_marker(squares)
+  #  squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
+  # end
 
-  def count_computer_marker(squares)
-    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
-  end
+  # def count_computer_marker(squares)
+  #  squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
+  # end
 
   def three_identical_markers?(squares)
     markers = squares.select(&:marked?).collect(&:marker)
     return false if markers.size != 3
     markers.uniq.size == 1
+  end
+
+  def two_identical_markers?(squares, marker)
+    markers = squares.select(&:marked?).collect(&:marker)
+    markers.count(marker) == 2 && markers.uniq.size == 1
   end
 
   def reset
@@ -49,6 +54,16 @@ class Board
       squares = @squares.values_at(*line)
       if three_identical_markers?(squares)
         return squares.first.marker
+      end
+    end
+    nil
+  end
+
+  def find_at_risk_square(marker)
+    WINNING_LINES.each do |line|
+      squares = @squares.values_at(*line)
+      if two_identical_markers?(squares, marker)
+         return @squares.select {|k, v| line.include?(k) && v.marker == ' '}.keys.first
       end
     end
     nil
@@ -96,16 +111,30 @@ class Square
 end
 
 class Player
-  attr_reader :marker
+  attr_reader :marker, :score
 
   def initialize(marker)
     @marker = marker
+    @score = 0
+  end
+
+  def won
+    @score += 1
+  end
+
+  def champion?
+    @score == TTTGame::CHAMPION_WINS
+  end
+
+  def reset_score
+    @score = 0
   end
 end
 
 class TTTGame
   HUMAN_MARKER = "X"
   COMPUTER_MARKER = "O"
+  CHAMPION_WINS = 3
   attr_reader :board, :human, :computer, :current_marker
 
   def initialize
@@ -128,31 +157,63 @@ class TTTGame
     loop do
       display_board
       player_move
-      display_result
+      display_champion_message
       break unless play_again?
-      reset
+      reset_champion
       display_play_again_message
     end
   end
 
   def player_move
     loop do
-      current_player_moves
-      break if board.someone_won? || board.full?
-      clear_screen_and_display_board if human_turn?
+      loop do
+        current_player_moves
+        break if board.someone_won? || board.full?
+        clear_screen_and_display_board if human_turn?
+      end
+      display_result
+      break if human.champion? || computer.champion?
+      any_key_to_continue
+      reset
     end
+  end
+
+  def pause(num)
+    sleep(num)
+  end
+
+  def any_key_to_continue
+    puts "Enter any key to continue...."
+    gets.chomp
   end
 
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!"
+    puts "The first to #{CHAMPION_WINS} wins is the CHAMPION!"
+    pause(3)
   end
 
   def display_goodbye_message
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
 
+  def joinor(array, char = ', ', str = 'or')
+    return array[0] if array.size == 1
+    return "#{array[0]} #{str} #{array[1]}" if array.size == 2
+    output = ''
+    array.each_with_index do |int, idx|
+      if idx == (array.size - 1)
+        output += "#{str} #{int}"
+      else
+        output += "#{int}#{char}"
+      end
+    end
+    output
+  end
+
   def human_moves
-    puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
+    # puts "Choose a square (#{board.unmarked_keys.join(', ')}): "
+    puts "Choose a square (#{joinor(board.unmarked_keys)}): "
     square = nil
     loop do
       square = gets.chomp.to_i
@@ -163,7 +224,24 @@ class TTTGame
   end
 
   def computer_moves
-    board[board.unmarked_keys.sample] = computer.marker
+    square = nil
+    square = computer_offense
+    if !square 
+      square = computer_defense
+    end
+
+    if !square
+      square = board.unmarked_keys.sample
+    end
+    board[square] = computer.marker
+  end
+
+  def computer_defense
+    board.find_at_risk_square(HUMAN_MARKER)
+  end
+
+  def computer_offense
+    board.find_at_risk_square(COMPUTER_MARKER)
   end
 
   def current_player_moves
@@ -178,6 +256,10 @@ class TTTGame
 
   def human_turn?
     @current_marker == HUMAN_MARKER
+  end
+
+  def clear
+    system 'clear'
   end
 
   def clear_screen_and_display_board
@@ -195,11 +277,25 @@ class TTTGame
   def display_result
     clear_screen_and_display_board
     case board.winning_marker
-    when HUMAN_MARKER then puts "You Won!"
-    when COMPUTER_MARKER then puts "Computer won!"
+    when HUMAN_MARKER
+      human.won 
+      puts "You Won!"
+    when COMPUTER_MARKER
+      computer.won 
+      puts "Computer won!"
     else
       puts "It's a tie!"
     end
+    display_scores
+  end
+
+  def display_scores
+    puts "The Scoreboard is....You: #{human.score} | Computer: #{computer.score}"
+  end
+
+  def display_champion_message
+    champion = (human.champion? ? "You" : "Computer")
+    puts "The Tic Tac Toe Champion is crowned: #{champion}!"
   end
 
   def play_again?
@@ -213,14 +309,16 @@ class TTTGame
     answer == 'y'
   end
 
-  def clear
-    system 'clear'
-  end
-
   def reset
     board.reset
     @current_marker = HUMAN_MARKER
     clear
+  end
+
+  def reset_champion
+    reset
+    human.reset_score
+    computer.reset_score
   end
 
   def display_play_again_message
